8ec3bdab65624fe28f223391a6ee366acdaf1db7
TheSylwio <thesylwio@gmail.com>
2021-11-07 21:43:14 +0100

    Create register user test basics

---
+++ tests/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class UserTest extends ApiTestCase {
+	public function testRegister(): void {
+		$response = static::createClient()->request('POST', '/api/register', [
+			'email' => 'exampleuser@example.com',
+			'password' => 'admin123'
+		]);
+
+		$this->assertEquals(201, $response->getStatusCode());
+	}
+}
