ace3a8ca6e7d3771529e5c27d9e08a53a74a4448
TheSylwio <thesylwio@gmail.com>
2021-11-07 21:44:51 +0100

    Correct request body

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,8 +7,10 @@ use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 class UserTest extends ApiTestCase {
 	public function testRegister(): void {
 		$response = static::createClient()->request('POST', '/api/register', [
-			'email' => 'exampleuser@example.com',
-			'password' => 'admin123'
+			'body' => [
+				'email' => 'exampleuser@example.com',
+				'password' => 'admin123',
+			],
 		]);
 
 		$this->assertEquals(201, $response->getStatusCode());
