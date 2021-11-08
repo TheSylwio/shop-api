959d251eba37dc1675e6c5e0bda2540a1540cc28
TheSylwio <thesylwio@gmail.com>
2021-11-09 00:03:32 +0100

    Update UserTest

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,17 +2,45 @@
 
 namespace App\Tests;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class UserTest extends ApiTestCase {
-	public function testRegister(): void {
-		$response = static::createClient()->request('POST', '/api/register', [
-			'body' => [
-				'email' => 'exampleuser@example.com',
-				'password' => 'admin123',
-			],
+class UserTest extends WebTestCase {
+	public function testAuthorization() {
+		$client = static::createClient();
+		$content = json_encode([
+			'email' => 'johndoe@example.com',
+			'password' => 'admin123'
 		]);
 
-		$this->assertEquals(201, $response->getStatusCode());
+		$client->request('POST', '/api/authentication_token', [], [], ['CONTENT_TYPE' => 'application/json'], $content);
+		$response = $client->getResponse();
+
+		$this->assertSame(200, $response->getStatusCode());
+		$this->assertJson($response->getContent());
+		$this->assertArrayHasKey('token', json_decode($response->getContent(), true));
+
+	}
+
+	public function testRegister(): void {
+		$client = static::createClient();
+		$userRepository = static::getContainer()->get(UserRepository::class);
+		$user = $userRepository->findOneBy(['email' => 'johndoe@example.com']);
+
+		$content = json_encode([
+			'email' => 'test@example.com',
+			'password' => 'test123',
+			'username' => 'testuser'
+		]);
+
+		$client->loginUser($user);
+		$client->request('POST', '/api/user/register', [], [], ['CONTENT_TYPE' => 'application/json'], $content);
+
+		$this->assertResponseIsSuccessful();
+		$this->assertNotNull($userRepository->findOneBy(['email' => 'test@example.com']));
+	}
+
+	protected function tearDown(): void {
+		parent::tearDown();
 	}
 }
